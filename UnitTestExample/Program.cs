6ffa42c23437b98d7691a289c00744da5980fc86
Patryk Milewski <mpatryk97@gmail.com>
2018-05-12 01:59:20 +0200

    Sample of unit testing - MSVS TestTools

---
+++ UnitTestExample/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+
+namespace UnitTestExample
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			bool?[] grid = new bool?[Width * Height];
+			bool player = true, lined = false;
+			int key = 0;
+
+			do
+			{
+				Console.Clear();
+				Console.WriteLine(GridToString(grid));
+
+				if ((key = Console.ReadKey().KeyChar - '1') < 0 || key > 8 || grid[key].HasValue)
+					continue;
+
+				grid[key] = player;
+				if (lined = IsLined(grid))
+					break;
+				player ^= true;
+
+			} while (!IsFull(grid));
+
+			Console.Clear();
+
+			Console.WriteLine(lined ? "Player '{0}' won" : "Draw!", player ? "x" : "y");
+			Console.ReadLine();
+		}
+
+		public static string GridToString(bool?[] grid)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < grid.Length;)
+			{
+				sb.Append(GridItemToChar(grid[i++], i.ToString()[0]));
+				if (i % Width == 0)
+					sb.Append('\n');
+			}
+			return sb.ToString();
+		}
+
+		public static string GridItemToChar(bool? item, char index) =>
+			!item.HasValue ? $" {index} "
+						   : item.Value == true ? " x " : " o ";
+
+		public static bool IsFull(bool?[] grid)
+		{
+			for (int i = 0; i < grid.Length; i++)
+				if (!grid[i].HasValue)
+					return false;
+			return true;
+		}
+
+		public static bool IsLined(bool?[] grid)
+		{
+			if (grid == null)
+				throw new ArgumentNullException(nameof(grid));
+			if (Width < 1)
+				throw new ApplicationException(nameof(Width));
+
+			for (int i = 0; i < Height; i++)
+				if (IsSameForAll(GridSplit(grid, i * Width, Width, k => k)))
+					return true;
+
+			for (int i = 0; i < Width; i++)
+				if (IsSameForAll(GridSplit(grid, i, Width, k => k * Width)))
+					return true;
+
+			return IsSameForAll(GridSplit(grid, 0, Width, k => k * Width + k))
+				|| IsSameForAll(GridSplit(grid, Width - 1, Width, k => k * Width - k));
+		}
+
+		public static bool?[] GridSplit(bool?[] grid, int startIndex, int count, Func<int, int> offset)
+		{
+			bool?[] chunk = new bool?[count];
+			for (int i = 0; i < count; i++)
+				chunk[i] = grid[startIndex + offset(i)];
+			return chunk;
+		}
+
+		public static bool IsSameForAll(bool?[] line)
+		{
+			if (!line[0].HasValue)
+				return false;
+			bool current = line[0].Value;
+			for (int i = 1; i < line.Length; i++)
+				if (!line[i].HasValue || line[i].Value != current)
+					return false;
+			return true;
+		}
+
+		internal static readonly int Width = 3;
+		internal static readonly int Height = 3;
+	}
+}
---
+++ UnitTestExampleTests/ProgramTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExample.Tests
+{
+	[TestClass()]
+	public class ProgramTests
+	{
+		[TestMethod()]
+		public void GridToStringTest()
+		{
+			Assert.IsTrue(Program.GridToString(new bool?[] { null, null, null }).CompareTo(" 1  2  3 \n") == 0, "First");
+
+			Assert.IsTrue(Program.GridToString(new bool?[] { true, null, false }).CompareTo(" x  2  o \n") == 0, "Second");
+
+			Assert.IsTrue(Program.GridToString(new bool?[] { null, false, null }).CompareTo(" 1  o  3 \n") == 0, "Third");
+		}
+
+		[TestMethod()]
+		public void GridItemToCharTest() { }
+		// Sprawdzane w GridToStringTest(), mozna by te funkcjonalnosci rozdzielic
+
+		[TestMethod()]
+		public void IsFullTest()
+		{
+			bool?[] test = new bool?[Width * Height];
+			for (int i = 0; i < Width; i++)
+				for (int j = 0; j < Height; j++)
+				{
+					Assert.AreEqual(
+						!Program.IsFull(test),
+						test.Contains(null)
+					);
+
+					test[i * Height + j] = Increment(test[i * Height + j]);
+				}
+
+			bool? Increment(bool? value) => value.HasValue
+				? value.Value ? false : (bool?)null
+				: true;
+		}
+
+		[TestMethod()]
+		public void IsLinedTest()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => Program.IsLined(null));
+
+			bool value = false;
+
+			bool?[] test = null;
+			for (int j = 0; j < 2; j++)
+			{
+				test = new bool?[Width * Height];
+				for (int i = 0; i < Height; i++)
+					test[Width * i + i] = value;
+				Assert.IsTrue(Program.IsLined(test), $"#1 Diagonal {j}");
+				value ^= true;
+			}
+
+			test[0] ^= true;
+			Assert.IsFalse(Program.IsLined(test), "#1 Inner check");
+
+			for (int j = 0; j < 2; j++)
+			{
+				test = new bool?[Width * Height];
+				for (int i = 0; i < Height; i++)
+					test[Width * Height - i - 1] = value;
+				Assert.IsTrue(Program.IsLined(test), $"#2 Diagonal {j}");
+				value ^= true;
+			}
+
+			test[test.Length - 1] ^= true;
+			Assert.IsFalse(Program.IsLined(test), "#2 Inner check");
+
+			for (int j = 0; j < Width; j++)
+			{
+				test = new bool?[Width * Height];
+				for (int i = 0; i < Height; i++)
+					test[i + j * Width] = value;
+				Assert.IsTrue(Program.IsLined(test), $"Rows {j}");
+				value ^= true;
+			}
+
+			for (int j = 0; j < Width; j++)
+			{
+				test = new bool?[Width * Height];
+				for (int i = 0; i < Height; i++)
+					test[i * Width + j] = value;
+				Assert.IsTrue(Program.IsLined(test), $"Columns {j}");
+				value ^= true;
+			}
+		}
+
+		[TestMethod()]
+		//[ExpectedException(typeof(IndexOutOfRangeException))]
+		public void GridSplitTest()
+		{
+			bool?[] test = new bool?[] { false, true, null };
+
+			Assert.IsFalse(Program.GridSplit(test, 0, 1, i => i)[0].Value, "0");
+			Assert.IsTrue(Program.GridSplit(test, 1, 1, i => i)[0].Value, "1");
+			Assert.IsFalse(Program.GridSplit(test, 2, 1, i => i)[0].HasValue, "2");
+			Assert.IsTrue(Program.GridSplit(test, 0, 2, i => i)[1].Value, "3");
+
+			// Mozna tu dodac jeszcze wiele przypadkow i petli
+			// Exception
+			Assert.ThrowsException<IndexOutOfRangeException>(
+				() => Program.GridSplit(test, 0, 4, i => i)
+			);
+		}
+
+		[TestMethod()]
+		public void IsSameForAllTest()
+		{
+			var trueValues = GenerateTrue();
+			foreach (var item in trueValues)
+				Assert.IsTrue(Program.IsSameForAll(item), "TrueTest");
+
+			foreach (var item in GenerateFalse(trueValues))
+				Assert.IsFalse(Program.IsSameForAll(item), "FalseTest");
+		}
+
+		private List<bool?[]> GenerateTrue()
+		{
+			List<bool?[]> values = new List<bool?[]>();
+			bool value = false;
+			for (int i = 0; i < 2; i++)
+			{
+				values.Add(new bool?[] { value, value, value });
+				value ^= true;
+			}
+			return values;
+		}
+
+		private List<bool?[]> GenerateFalse(List<bool?[]> trueValues)
+		{
+			List<bool?[]> values = new List<bool?[]>();
+			var bools = new bool?[] { false, false, false };
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					for (int k = 0; k < 2; k++)
+					{
+						values.Add(bools.ToArray());
+						bools[2] ^= true;
+					}
+					bools[1] ^= true;
+				}
+				bools[0] ^= true;
+			}
+
+			for (int i = 0; i < values.Count; i++)
+				if (trueValues.Any(values[i].SequenceEqual))
+					values.RemoveAt(i);
+			return values;
+		}
+
+		private const int Width = 3;
+		private const int Height = 3;
+	}
+}
